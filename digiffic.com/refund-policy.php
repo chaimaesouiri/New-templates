07df79f727bda6d77a916149692ae6785182076a
Chaimae Souiri <77145854+chaimaesouiri@users.noreply.github.com>
2023-11-13 13:02:06 +0100

    Add files via upload

---
+++ digiffic.com/refund-policy.php
@@ -0,0 +1,209 @@
+<?php include_once ('website.php'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Digiffic | Refund Policy</title>
+    <link rel="shortcut icon" href="<?=$host?>/hubfs/Group%2045%20(1).png">
+    <meta name="description" content="At Digiffic, your success is our goal. Every day, we create digital solutions that empower business growth for our clients from all over the world.">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;900&amp;display=swap" rel="stylesheet">
+    <script src="/hs/hsstatic/jquery-libs/static-1.1/jquery/jquery-1.7.1.js"></script>
+    <meta property="og:description" content="At Digiffic, your success is our goal. Every day, we create digital solutions that empower business growth for our clients from all over the world.">
+    <meta property="og:title" content="Digiffic | your digital partner &amp; #1 web marketing agency">
+    <meta name="twitter:description" content="At Digiffic, your success is our goal. Every day, we create digital solutions that empower business growth for our clients from all over the world.">
+    <meta name="twitter:title" content="Digiffic | your digital partner &amp; #1 web marketing agency">
+    <style>
+        a.cta_button {
+            -moz-box-sizing: content-box !important;
+            -webkit-box-sizing: content-box !important;
+            box-sizing: content-box !important;
+            vertical-align: middle
+        }
+        
+        .hs-breadcrumb-menu {
+            list-style-type: none;
+            margin: 0px 0px 0px 0px;
+            padding: 0px 0px 0px 0px
+        }
+        
+        .hs-breadcrumb-menu-item {
+            float: left;
+            padding: 10px 0px 10px 10px
+        }
+        
+        .hs-breadcrumb-menu-divider:before {
+            content: '›';
+            padding-left: 10px
+        }
+        
+        .hs-featured-image-link {
+            border: 0
+        }
+        
+        .hs-featured-image {
+            float: right;
+            margin: 0 0 20px 20px;
+            max-width: 50%
+        }
+        
+        @media (max-width: 568px) {
+            .hs-featured-image {
+                float: none;
+                margin: 0;
+                width: 100%;
+                max-width: 100%
+            }
+        }
+        
+        .hs-screen-reader-text {
+            clip: rect(1px, 1px, 1px, 1px);
+            height: 1px;
+            overflow: hidden;
+            position: absolute !important;
+            width: 1px
+        }
+    </style>
+    <link rel="stylesheet" href="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/81714954348/1660208556553/Digiffic-2022/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/81624152748/1660216030735/Digiffic-2022/css/main.min.css">
+    <link rel="stylesheet" href="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/81712787292/1684411102805/Digiffic-2022/css/style.min.css">
+    <link rel="stylesheet" href="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/94631623714/1670861135840/Digiffic-2022/css/additional-style.min.css">
+    <!-- /Added by GoogleTagManager integration -->
+    <link rel="canonical" href="<?=$host?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:url" content="<?=$host?>">
+    <meta name="twitter:card" content="summary">
+    <meta http-equiv="content-language" content="en">
+    <meta name="generator" content="HubSpot">
+</head>
+<body class="footer-style-default">
+<div class="body-wrapper hs-content-id-81709491617 hs-site-page page ">
+<div data-global-resource-path="">
+<header class="header">
+    <div class="header__logo"> <span id="hs_cos_wrapper_logo_image" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_linked_image" style="" data-hs-cos-general-type="widget" data-hs-cos-type="linked_image"><a href="<?=$host?>/" id="hs-link-logo_image" style="border-width:0px;border:0px;"><img src="<?=$host?>/hubfs/Digiffic%202022/logo.svg" class="hs-image-widget " style="width:300px;border-width:0px;border:0px;" width="300" alt="Logo" title="Logo"></a></span> </div>
+    <div class="header__content js-nav-holder">
+        <div class="nav"> <span id="hs_cos_wrapper_menu" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_menu" style="" data-hs-cos-general-type="widget" data-hs-cos-type="menu"><div id="hs_menu_wrapper_menu" class="hs-menu-wrapper active-branch flyouts hs-menu-flow-horizontal" role="navigation" data-sitemap-name="default" data-menu-id="81624702507" aria-label="Navigation Menu">
+         <ul role="menu" class="active-branch">
+          <li class="hs-menu-item hs-menu-depth-1 active active-branch" role="none"><a href="<?=$host?>" role="menuitem">Home</a></li>
+          <li class="hs-menu-item hs-menu-depth-1" role="none"><a href="<?=$host?>/#contact" role="menuitem">Contact</a></li>
+         </ul>
+        </div></span>
+        </div>
+        <!-- /.nav -->
+    </div>
+    <!-- /.header__content -->
+    <div class="header__actions visible-xs-block">
+        <a href="#" class="nav-trigger js-nav-trigger"> <span></span> </a>
+    </div>
+    <!-- /.header__actions visible-xs-block -->
+</header>
+<!-- /.header -->
+</div>
+<div class="main">
+<style>.hero {padding: 9.9rem 0 5.8rem;}</style>
+<main>
+    <div class="container-fluid">
+        <div class="row-fluid-wrapper">
+            <div class="row-fluid">
+                <div class="span12 widget-span widget-type-cell " style="" data-widget-type="cell" data-x="0" data-w="12">
+                    <div class="row-fluid-wrapper row-depth-1 row-number-1 dnd-section">
+                        <div class="row-fluid ">
+                            <div class="span12 widget-span widget-type-cell feature_post-column-1-padding dnd-column" style="" data-widget-type="cell" data-x="0" data-w="12">
+                                <div class="row-fluid-wrapper row-depth-1 row-number-2 dnd-row">
+                                    <div class="row-fluid ">
+                                        <div class="span12 widget-span widget-type-custom_widget dnd-module" style="" data-widget-type="custom_widget" data-x="0" data-w="12">
+                                            <div id="hs_cos_wrapper_feature_post-module-2" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_module" style="" data-hs-cos-general-type="widget" data-hs-cos-type="module">
+                                                <div class="hero" id="hero">
+                                                   <h2>Refund Policy</h2>
+<p>Thank you for choosing <?=$ihost?> ! We value your satisfaction and strive to provide the best possible experience with our software services. In the event that you are not entirely satisfied with your purchase, we’re here to help.</p>
+    <p>Subscription Cancellation and Refunds</p>
+    <p>We offer a 30-day money-back guarantee on our monthly subscription plans. If you are dissatisfied with our services within this period, please contact our customer support team at support@<?=$ihost?> to request a cancellation and refund.</p>
+    <p>To be eligible for a refund, you must provide us with a valid reason for your dissatisfaction and demonstrate that you have made a reasonable effort to resolve the issue with our support team.</p>
+    <p>Refund Process</p>
+    <p>To initiate a refund request, please follow these steps:</p>
+    <ol>
+        <li>Contact our customer support team at support@<?=$ihost?> within the specified refund period.</li>
+        <li>Provide your order details, including the email address used for the purchase and any relevant information related to your dissatisfaction.</li>
+        <li>Our support team will review your request and may request additional information if necessary.</li>
+        <li>If your refund request is approved, we will process the refund within 7 days and notify you via email.</li>
+    </ol>
+    <p>Please note the following important points:</p>
+    <ol>
+        <li>Refunds will be issued using the same payment method used for the original purchase.</li>
+        <li>Processing times for refunds may vary depending on your payment provider.</li>
+        <li>Any taxes or fees associated with the refund will be deducted from the refund amount, if applicable.</li>
+    </ol>
+    <p>Exclusions</p>
+    <p>Please be aware that certain situations are excluded from our refund policy. We do not provide refunds in the following cases:</p>
+    <ol>
+        <li>After the specified refund period has expired.</li>
+        <li>If you have violated our terms of service or engaged in any fraudulent activity.</li>
+        <li>If your dissatisfaction is due to circumstances beyond our control, such as third-party service interruptions, force majeure events, or acts of nature.</li>
+        <li>If you have already received a refund for a previous purchase of our services.</li>
+    </ol>
+    <p>Contact Us</p>
+    <p>If you have any questions or concerns regarding our refund policy, please contact us at support@<?=$ihost?> , Our support team is available to assist you and provide further clarification.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+</div>
+<div>
+<footer class="footer">
+    <div class="shell">
+        <div class="footer__inner">
+            <div class="footer__logo"> <span id="hs_cos_wrapper_footer_logo" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_linked_image" style="" data-hs-cos-general-type="widget" data-hs-cos-type="linked_image"><img src="<?=$host?>/hubfs/Digiffic%202022/logo-sign-light.svg" class="hs-image-widget " style="border-width:0px;border:0px;" alt="Logo" title="Logo"></span> </div>
+            <div class="footer__nav"> 
+            <span id="hs_cos_wrapper_menu" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_menu" style="" data-hs-cos-general-type="widget" data-hs-cos-type="menu"><div id="hs_menu_wrapper_menu" class="hs-menu-wrapper active-branch flyouts hs-menu-flow-horizontal" role="navigation" data-sitemap-name="default" data-menu-id="81722143147" aria-label="Navigation Menu">
+             <ul role="menu" class="active-branch">
+              <li class="hs-menu-item hs-menu-depth-1 active active-branch" role="none"><a href="<?=$host?>" role="menuitem">Home</a></li>
+              <li class="hs-menu-item hs-menu-depth-1" role="none"><a href="<?=$host?>/terms-of-service" role="menuitem">Terms Of Service</a></li>
+              <li class="hs-menu-item hs-menu-depth-1" role="none"><a href="<?=$host?>/privacy" role="menuitem">Privacy Policy</a></li>
+              <li class="hs-menu-item hs-menu-depth-1" role="none"><a href="<?=$host?>/refund-policy" role="menuitem">Refund policy</a></li>
+              <li class="hs-menu-item hs-menu-depth-1" role="none"><a href="<?=$host?>/delivery-policy" role="menuitem">delivery policy</a></li>
+              <li class="hs-menu-item hs-menu-depth-1" role="none"><a href="<?=$host?>/about" role="menuitem">about us</a></li>
+             </ul>
+            </div>
+            </span> 
+           </div><br><br>
+            <div id="hs_cos_wrapper_module_16601420264205" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_module" style="" data-hs-cos-general-type="widget" data-hs-cos-type="module">
+                <div class="socials">
+                    <ul>
+                        <li>
+                            <a href="https://www.linkedin.com/" target="_blank" rel="nofollow noopener"> <img src="<?=$host?>/hubfs/Digiffic%202022/icon-linkedin.svg" alt="icon-linkedin" loading="lazy" width="39" height="30"> Linked In </a>
+                        </li>
+                        <li>
+                            <a href="" target="_blank" rel="nofollow noopener"> <img src="<?=$host?>/hs-fs/hubfs/Clutch%20(1).png?width=70&amp;height=70&amp;name=Clutch%20(1).png" alt="Clutch (1)" loading="lazy" width="70" height="70"> </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank" rel="nofollow noopener"> <img src="<?=$host?>/hubfs/Digiffic%202022/icon-facebook.svg" alt="icon-facebook" loading="lazy" width="39" height="30"> Facebook </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+    <!-- HubSpot performance collection script -->
+    <script defer src="https://static.hsappstatic.net/content-cwv-embed/static-1.388/embed.js"></script>
+    <script src="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/81713632007/1660208720714/Digiffic-2022/js/plugins/jquery-3.6.0.min.js"></script>
+    <script src="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/81713631875/1660208530883/Digiffic-2022/js/plugins/swiper-bundle.min.js"></script>
+    <script src="<?=$host?>/hs-fs/hub/6087269/hub_generated/template_assets/81624920122/1670593816794/Digiffic-2022/js/main.min.js"></script>
+    <script src="/hs/hsstatic/keyboard-accessible-menu-flyouts/static-1.17/bundles/project.js"></script>
+    <script src="/hs/hsstatic/cos-i18n/static-1.53/bundles/project.js"></script>
+    <script data-hs-allowed="true" src="/_hcms/forms/v2.js"></script>
+</body>
+</html>
